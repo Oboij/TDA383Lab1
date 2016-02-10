fc9de54c2586cf3be10dcd14876e9a9333b099cb
MonsterMannen <viktorsvensson94@gmail.com>
2016-02-10 18:53:21 +0100

    adding file, now do cool stuff with it!

---
+++ secretFile.java
@@ -0,0 +1,11 @@
+public class secretFile {
+	private static String secret = "shhh";
+
+	public static void main(String[] args){
+		System.out.println(secret);
+
+		//brancha in något
+		//eller pull requesta :^)
+		//skumma grejser
+	}
+}
